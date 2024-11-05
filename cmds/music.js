7eb6603812917e4c01205e08d5d40475e69c2984
arelk68 <artieelkins@hotmail.com>
2024-11-05 15:09:46 +0000

    Music Added

---
+++ cmds/music.js
@@ -0,0 +1,6 @@
+
+function playmusic() {
+    var audio = new Audio('Music.mp3');
+    audio.setAttribute('autoplay', 'autoplay');
+    audio.play();
+}
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 
 function setup() {
   setupLandscape()
+  playmusic()
 }
 
 function draw() {
